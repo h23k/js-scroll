ed08140b86d374a1a0305c1182169defc4275d77
hiro <hi.kanno23@gmail.com>
2020-06-03 17:28:55 +0900

    fixed bug for mobile.

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 import style from './style.scss';
 
+document.addEventListener('scroll', () => {
+  const toTop = document.querySelector('.to-top');
+  const scroll = document.querySelector('.scroll');
+  const contents = document.querySelectorAll('.content');
+  const lastContent = contents[contents.length - 1];
+  const top = Math.floor(lastContent.getBoundingClientRect().top);
+
+  // const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  // const clientHeight = document.documentElement.clientHeight;
+
+  if (scrollTop <= 0) {
+    toTop.classList.add('to-top--hide');
+  } else {
+    toTop.classList.remove('to-top--hide');
+  }
+
+  if (top <= 0) {
+    scroll.classList.add('scroll--hide');
+  } else {
+    scroll.classList.remove('scroll--hide');
+  }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   const toTop = document.querySelector('.to-top');
   const scroll = document.querySelector('.scroll');
-
-  document.addEventListener('scroll', () => {
-    const scrollHeight = document.documentElement.scrollHeight;
-    const scrollTop = document.documentElement.scrollTop;
-    const clientHeight = document.documentElement.clientHeight;
-
-    if (scrollTop <= 0) {
-      toTop.classList.add('to-top--hide');
-    } else {
-      toTop.classList.remove('to-top--hide');
-    }
-
-    if (scrollHeight <= scrollTop + clientHeight) {
-      scroll.classList.add('scroll--hide');
-    } else {
-      scroll.classList.remove('scroll--hide');
-    }
-  });
 
   toTop.addEventListener('click', () => {
     window.scrollTo({
@@ -33,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const contents = document.querySelectorAll('.content');
 
     for (var i = 0; i < contents.length; i++) {
-      const targetTop = Math.abs(contents[i].getBoundingClientRect().top);
+      const targetTop = Math.floor(contents[i].getBoundingClientRect().top);
       if (0 < targetTop) {
         window.scrollBy({
           "top": targetTop,
